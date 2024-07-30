9d964f6f35fcf4b8793470c0efd609ef8683291c
Nosorozhek <nosorozhek239@gmail.com>
2024-07-30 23:44:31 +0300

    Fixed tests 😎

--- src/test/kotlin/ru/shiroforbes/ApplicationTest.kt
+++ src/test/kotlin/ru/shiroforbes/ApplicationTest.kt
@@ -2,13 +2,9 @@
 
 package ru.shiroforbes
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
 import io.ktor.server.testing.*
 import ru.shiroforbes.config.configureApp
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
@@ -16,10 +12,6 @@ class ApplicationTest {
         testApplication {
             application {
                 configureApp()
-            }
-            client.get("/").apply {
-                assertEquals(HttpStatusCode.OK, status)
-                assertEquals("Hello World!", bodyAsText())
             }
         }
 }
