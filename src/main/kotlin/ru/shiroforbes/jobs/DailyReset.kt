368bb37fad88b09ed5bca7fa06385e045c3d747a
Sasha239 <alex.bogdanov05@mail.ru>
2024-08-01 19:31:20 +0300

    Small update for transactions. Also exercises and "beating" now gives money

--- src/main/kotlin/ru/shiroforbes/jobs/DailyReset.kt
+++ src/main/kotlin/ru/shiroforbes/jobs/DailyReset.kt
@@ -1,30 +1,43 @@
 package ru.shiroforbes.jobs
 
 import kotlinx.coroutines.runBlocking
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.quartz.Job
 import org.quartz.JobExecutionContext
-import ru.shiroforbes.service.DbStudentService
-import ru.shiroforbes.service.GroupService
-import ru.shiroforbes.service.StudentService
+import ru.shiroforbes.model.Student
+import ru.shiroforbes.model.Transaction
+import ru.shiroforbes.service.*
+
 
 class DailyResetExercise(
     val groupService: GroupService? = null,
-    val studentService: StudentService? = DbStudentService,
+    private val studentService: StudentService? = DbStudentService,
+    private val transactionService: TransactionService? = DbTransactionService,
 ) : Job {
     override fun execute(context: JobExecutionContext?) {
         runBlocking {
-            studentService?.updateAllStudentsExercised(false)
+            transactionService!!.SendMoneyByCondition(studentService!!.getAllStudents(), amount = 50) {
+                this.isExercised == true
+            }
+            studentService.updateAllStudentsExercised(false)
         }
     }
 }
 
 class DailyResetBeat(
     val groupService: GroupService? = null,
-    val studentService: StudentService? = DbStudentService,
+    private val studentService: StudentService? = DbStudentService,
+    private val transactionService: TransactionService? = DbTransactionService,
 ) : Job {
     override fun execute(context: JobExecutionContext?) {
         runBlocking {
-            studentService?.updateAllStudentsBeaten(false)
+            transactionService!!.SendMoneyByCondition(studentService!!.getAllStudents(), amount = 50) {
+                this.isBeaten == true
+            }
+            studentService.updateAllStudentsBeaten(false)
         }
     }
 }
