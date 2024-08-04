66aa91a2a4c1569e5fa8c3a9bff47a6282aaf4b9
Nosorozhek <nosorozhek239@gmail.com>
2024-08-04 05:04:03 +0300

    Added mock data and fixed Google Sheets service. Added progressbar to the login page. Added column rating+wealth to the menu.

--- src/main/resources/static/scripts/menu.js
+++ src/main/resources/static/scripts/menu.js
@@ -2,18 +2,17 @@ const countrysideButton = document.getElementById("countryside");
 const urbanButton = document.getElementById("urban");
 const countrysideContent = document.getElementById("countrysideContent")
 const urbanContent = document.getElementById("urbanContent")
-const ratingDownloadButton = document.getElementById("ratingDownloadButton")
 
-countrysideButton.addEventListener("click",() => {
+countrysideButton.addEventListener("click", () => {
         countrysideButton.classList.add("fill");
         urbanButton.classList.remove("fill");
-    ratingDownloadButton.onclick =
+
         countrysideContent.hidden = false;
     urbanContent.hidden = true;
     }
 )
 
-urbanButton.addEventListener("click",() => {
+urbanButton.addEventListener("click", () => {
         urbanButton.classList.add("fill");
         countrysideButton.classList.remove("fill");
     countrysideContent.hidden = true;
