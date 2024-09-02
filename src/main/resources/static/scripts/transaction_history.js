5d04b3240f1935432ab764aa269db8f47a4c1494
Sasha239 <alex.bogdanov05@mail.ru>
2024-09-02 23:37:54 +0300

    Access to transactions from profile

--- src/main/resources/static/scripts/transaction_history.js
+++ src/main/resources/static/scripts/transaction_history.js
@@ -2,8 +2,8 @@ const removeButtons = document.getElementsByClassName("remove-button")
 
 for (let i = 0; i < removeButtons.length; i++) {
     removeButtons[i].addEventListener("click", () => {
-        console.log(removeButtons[i].childNodes[1].textContent)
-        const transactionId = removeButtons[i].childNodes[1].textContent
+        console.log(removeButtons[i].childNodes)
+        const transactionId = removeButtons[i].id.substring(7)
         fetch("/transactions/delete/" + transactionId, {
             method: "POST",
             headers: {'Content-Type': 'application/json'},
