58e9cb9c12bf98f289d8256d30bd79541c4a9fdd
Nosorozhek <nosorozhek239@gmail.com>
2025-02-08 19:23:31 +0300

    Implemented AchievementService

--- src/main/kotlin/ru/shiroforbes/database/Init.kt
+++ src/main/kotlin/ru/shiroforbes/database/Init.kt
@@ -1,11 +1,14 @@
 package ru.shiroforbes.database
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils.create
 import org.jetbrains.exposed.sql.SchemaUtils.drop
+import org.jetbrains.exposed.sql.batchInsert
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -57,14 +60,14 @@ fun main() {
     )
 
     transaction(database) {
-        exec("DROP TYPE IF EXISTS rights CASCADE;\n")
-        exec("CREATE TYPE rights AS ENUM ('Admin', 'Teacher', 'Student');\n")
-        exec("DROP TYPE IF EXISTS semester CASCADE;\n")
-        exec("CREATE TYPE semester AS ENUM ('Semester2', 'Semesters12');\n")
+        exec("DROP TYPE IF EXISTS \"rights\" CASCADE;\n")
+        exec("CREATE TYPE \"rights\" AS ENUM ('Admin', 'Teacher', 'Student');\n")
+        exec("DROP TYPE IF EXISTS \"semester\" CASCADE;\n")
+        exec("CREATE TYPE \"semester\" AS ENUM ('Semester2', 'Semesters12');\n")
         exec("DROP TYPE IF EXISTS \"group\" CASCADE;\n")
         exec("CREATE TYPE \"group\" AS ENUM ('Countryside', 'Urban');\n")
 
-        drop(
+        val allTables = arrayOf(
             UserTable,
             AdminTable,
             TeacherTable,
@@ -72,15 +75,30 @@ fun main() {
             RatingTable,
             AchievementTable,
         )
-        create(
-            UserTable,
-            AdminTable,
-            TeacherTable,
-            StudentTable,
-            RatingTable,
-            AchievementTable,
-        )
-        fetchGoogleSheets("ShV!A2:N", ConversionClassStudent::class).forEach { student ->
+
+        drop(*allTables)
+        create(*allTables)
+    }
+
+    runBlocking {
+        launch {
+            val students = fetchGoogleSheets("ShV!A2:N", ConversionClassStudent::class)
+            addStudents(database, students)
+        }
+        launch {
+            val teachers = fetchGoogleSheets("Teachers!A2:D", ConversionClassTeacher::class)
+            addTeachers(database, teachers)
+        }
+        launch {
+            val admins = fetchGoogleSheets("Admins!A2:D", ConversionClassAdmin::class)
+            addAdmins(database, admins)
+        }
+    }
+}
+
+fun addStudents(database: Database, students: List<ConversionClassStudent>) =
+    transaction(database) {
+        students.forEach { student ->
             val id =
                 StudentTable.insertAndGetId {
                     it[name] = student.name
@@ -88,7 +106,6 @@ fun main() {
                     it[password] = student.password
                     it[group] = student.group
                 }
-
             for (s in Semester.entries) RatingTable.insert {
                 it[semester] = s
                 it[date] = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
@@ -116,36 +133,35 @@ fun main() {
                 it[rights] = Rights.Student
             }
         }
+    }
 
-        fetchGoogleSheets("Teachers!A2:D", ConversionClassTeacher::class).forEach { teacher ->
-            TeacherTable.insert {
-                it[name] = teacher.name
-                it[login] = teacher.login
-                it[password] = teacher.password
-            }
-
-            UserTable.insert {
-                it[login] = teacher.login
-                it[password] = teacher.password
-                it[rights] = Rights.Teacher
-            }
+fun addTeachers(database: Database, teachers: List<ConversionClassTeacher>) =
+    transaction(database) {
+        TeacherTable.batchInsert(teachers) { teacher ->
+            this[TeacherTable.name] = teacher.name
+            this[TeacherTable.login] = teacher.login
+            this[TeacherTable.password] = teacher.password
         }
-
-        fetchGoogleSheets("Admins!A2:D", ConversionClassAdmin::class).forEach { admin ->
-            AdminTable.insert {
-                it[name] = admin.name
-                it[login] = admin.login
-                it[password] = admin.password
-            }
-
-            UserTable.insert {
-                it[login] = admin.login
-                it[password] = admin.password
-                it[rights] = Rights.Admin
-            }
+        UserTable.batchInsert(teachers) { teacher ->
+            this[UserTable.login] = teacher.login
+            this[UserTable.password] = teacher.password
+            this[UserTable.rights] = Rights.Teacher
         }
     }
-}
+
+fun addAdmins(database: Database, admins: List<ConversionClassAdmin>) =
+    transaction(database) {
+        AdminTable.batchInsert(admins) { admin ->
+            this[AdminTable.name] = admin.name
+            this[AdminTable.login] = admin.login
+            this[AdminTable.password] = admin.password
+        }
+        UserTable.batchInsert(admins) { admin ->
+            this[UserTable.login] = admin.login
+            this[UserTable.password] = admin.password
+            this[UserTable.rights] = Rights.Admin
+        }
+    }
 
 private fun <T : Any> fetchGoogleSheets(
     range: String,
